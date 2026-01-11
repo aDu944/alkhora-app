1d1114ebeb8ee56ea114208dcf6edc5984b7b100
aDu944 <a.du944@gmail.com>
2026-01-11 19:23:03 +0300

    Update compiled Python bytecode for management_dashboard API module.

---
+++ management_dashboard/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(
+    name="management_dashboard",
+    version="1.0.0",
+    description="ALKHORA Company App - Custom ERPNext extensions and workspaces",
+    author="ALKHORA",
+    author_email="support@alkhora.co",
+    packages=[
+        "management_dashboard",
+        "management_dashboard.api",
+        "management_dashboard.config",
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
+
