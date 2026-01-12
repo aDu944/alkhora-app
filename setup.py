042884ae0fce967582033152834cdc725b6b7bd5
aDu944 <a.du944@gmail.com>
2026-01-12 12:12:56 +0300

    Refactor pyproject.toml and setup.py to simplify project metadata, update author email, and remove unnecessary files for a cleaner package structure.

--- setup.py
+++ setup.py
@@ -1,27 +1,13 @@
-from setuptools import setup
-
+from setuptools import setup, find_packages
 
 setup(
     name="management_dashboard",
     version="1.0.0",
-    description="ALKHORA Company App - Custom ERPNext extensions and workspaces",
+    description="ALKHORA Company App",
     author="ALKHORA",
-    author_email="support@alkhora.co",
-    packages=[
-        "management_dashboard",
-        "management_dashboard.api",
-        "management_dashboard.config",
-    ],
+    author_email="support@alkhora.com",
+    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-    ],
 )
-
